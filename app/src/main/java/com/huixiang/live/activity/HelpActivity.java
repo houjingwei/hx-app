52e6609423387c28165e50d182b58222fae08c90
WangWeiJie <wwjd163yx@163.com>
2016-05-13 20:16:47 +0800

    帮助中心activity

---
+++ app/src/main/java/com/huixiang/live/activity/HelpActivity.java
@@ -0,0 +1,15 @@
+package com.huixiang.live.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.huixiang.live.R;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
