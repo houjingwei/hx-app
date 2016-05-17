fd14f1b3826515647e7583188447afcb88a029b8
jg_android <541016407@qq.com>
2016-05-17 11:48:03 +0800

    add apm application and noDouble control

--- app/src/main/java/com/huixiang/live/activity/BaseActivity.java
+++ app/src/main/java/com/huixiang/live/activity/BaseActivity.java
@@ -2,18 +2,29 @@ package com.huixiang.live.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.oneapm.agent.android.module.analysis.AnalysisModule;
 
 public class BaseActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AnalysisModule.onResume();
+    }
 
 
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        AnalysisModule.onPause();
+    }
 
 
 }
