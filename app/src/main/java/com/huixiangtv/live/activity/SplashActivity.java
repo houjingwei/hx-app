a7bcc20beefcab07982846db949ec3603ccf9371
jg_android <541016407@qq.com>
2016-06-24 18:38:20 +0800

    ad

--- app/src/main/java/com/huixiangtv/live/activity/SplashActivity.java
+++ app/src/main/java/com/huixiangtv/live/activity/SplashActivity.java
@@ -86,7 +86,7 @@ public class SplashActivity extends Activity{
                     }
                 }
 
-                
+
                 @Override
                 public void onFailure(ServiceException e) {
                     super.onFailure(e);
