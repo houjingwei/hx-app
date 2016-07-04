1056e4c3899988e0d014785d6082bbad176ecefc
jg_android <541016407@qq.com>
2016-07-04 16:37:04 +0800

    add roundimageview

--- app/src/main/java/com/huixiangtv/live/fragment/FragmentCircle.java
+++ app/src/main/java/com/huixiangtv/live/fragment/FragmentCircle.java
@@ -120,105 +120,10 @@ public class FragmentCircle extends Fragment {
     public void onResume() {
         super.onResume();
     }
-
-
-    private List<Dynamic> getData(){
-
-        List<Dynamic> dynamics = new ArrayList<>();
-
-        Dynamic dynamic1 = new Dynamic();
-        dynamic1.setDynamicId("11");
-        dynamic1.setCommentCount("5");
-        DynamicComment cm = new DynamicComment();
-        cm.setContent("hahahaha");
-        cm.setNickName("szj:");
-
-        DynamicComment cm1 = new DynamicComment();
-        cm1.setContent("今天在干嘛？");
-        cm1.setNickName("兰艳:");
-
-        DynamicComment cm2 = new DynamicComment();
-        cm2.setContent("？");
-        cm2.setNickName("兰艳:");
-
-
-        DynamicpPraise praise = new DynamicpPraise();
-        praise.setPhoto("http://c.hiphotos.baidu.com/image/h%3D200/sign=b7453bef44166d222777129476220945/b3b7d0a20cf431ad2951ff1d4c36acaf2edd9817.jpg");
-
-
-        DynamicpPraise praise1 = new DynamicpPraise();
-        praise1.setPhoto("http://c.hiphotos.baidu.com/image/h%3D200/sign=b7453bef44166d222777129476220945/b3b7d0a20cf431ad2951ff1d4c36acaf2edd9817.jpg");
-
-        List<DynamicpPraise> praiseList= new ArrayList<>();
-        praiseList.add(praise); praiseList.add(praise1);praiseList.add(praise1);praiseList.add(praise1);praiseList.add(praise1);praiseList.add(praise1);praiseList.add(praise1);praiseList.add(praise1);praiseList.add(praise1);praiseList.add(praise1);
-        dynamic1.setPraises(praiseList);
-
-        List<DynamicImage> imagesList = new ArrayList<>();
-        DynamicImage images = new DynamicImage();
-        images.setBig("http://c.hiphotos.baidu.com/image/h%3D200/sign=b7453bef44166d222777129476220945/b3b7d0a20cf431ad2951ff1d4c36acaf2edd9817.jpg");
-        images.setSmall("http://c.hiphotos.baidu.com/image/h%3D200/sign=b7453bef44166d222777129476220945/b3b7d0a20cf431ad2951ff1d4c36acaf2edd9817.jpg");
-        imagesList.add(images);
-        dynamic1.setImages(imagesList);
-
-
-        List<DynamicComment> cmList= new ArrayList<>();
-        cmList.add(cm);
-        cmList.add(cm1);  cmList.add(cm2);
-        dynamic1.setContent("xixixi");
-        dynamic1.setComments(cmList);
-        dynamic1.setNickName("ly");
-        dynamic1.setPhoto("https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=413574418,3633313838&fm=80");
-
-        dynamics.add(dynamic1);
-
-
-
-        Dynamic dynamic2 = new Dynamic();
-        dynamic2.setCommentCount("5");
-        dynamic2.setDynamicId("22");
-        DynamicComment c3 = new DynamicComment();
-        c3.setContent("xxxxx");
-        c3.setNickName("Andy:");
-
-        DynamicComment cm4 = new DynamicComment();
-        cm4.setContent("今天在干嘛？");
-        cm4.setNickName("艳子:");
-
-        DynamicComment cm5 = new DynamicComment();
-        cm5.setContent("我回家了");
-        cm5.setNickName("艳子:");
-
-
-
-
-
-
-        List<DynamicComment> cmList1= new ArrayList<>();
-        cmList1.add(cm);
-        cmList1.add(cm1);  cmList1.add(cm2);
-        dynamic2.setContent("xixixi");
-        dynamic2.setComments(cmList);
-        dynamic2.setNickName("Andy");
-        dynamic2.setPhoto("http://c.hiphotos.baidu.com/image/h%3D200/sign=b7453bef44166d222777129476220945/b3b7d0a20cf431ad2951ff1d4c36acaf2edd9817.jpg");
-        List<DynamicpPraise> praiseList1= new ArrayList<>();
-        praiseList1.add(praise); praiseList1.add(praise1);praiseList1.add(praise1);
-        dynamic2.setPraises(praiseList1);
-        dynamics.add(dynamic2);
-
-
-        return dynamics;
-
-    }
-
     /**
      *  获取圈子列表
      */
     private void bindDynamicInfo() {
-
-
-//        adapter.addList(getData());
-//
-//        refreshView.onRefreshComplete();
 
         Map<String, String> paramsMap = new HashMap<String, String>();
         paramsMap.put("page", page + "");
