17777364440346f95a0e835c433dab2cd52a34fa
zijing <houjingwei@163.com>
2016-07-27 01:24:10 +0800

    1.系统消息监听，未读数量提醒 2.申请加入群组的用户列表，同意加入群组 3.聊天页面会话监听实现 4.新朋友用户列表

---
+++ app/src/main/java/com/huixiangtv/liveshow/activity/NewApplyFriendActivity.java
@@ -0,0 +1,142 @@
+package com.huixiangtv.liveshow.activity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+import com.huixiangtv.liveshow.Api;
+import com.huixiangtv.liveshow.Constant;
+import com.huixiangtv.liveshow.R;
+import com.huixiangtv.liveshow.adapter.NewApplyFriendAdapter;
+import com.huixiangtv.liveshow.model.Friend;
+import com.huixiangtv.liveshow.service.RequestUtils;
+import com.huixiangtv.liveshow.service.ResponseCallBack;
+import com.huixiangtv.liveshow.service.ServiceException;
+import com.huixiangtv.liveshow.ui.CommonTitle;
+import com.huixiangtv.liveshow.utils.CommonHelper;
+import com.huixiangtv.liveshow.utils.ForwardUtils;
+import com.huixiangtv.liveshow.utils.widget.WidgetUtil;
+
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NewApplyFriendActivity extends BaseBackActivity {
+
+    @ViewInject(R.id.myTitle)
+    CommonTitle commonTitle;
+
+    @ViewInject(R.id.listView)
+    private SwipeMenuListView mListView;
+
+    NewApplyFriendAdapter adapter ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_apply_friend);
+        x.view().inject(this);
+        initView();
+        loadData();
+    }
+
+    String page = "1";
+    private void loadData() {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("page",page);
+        params.put("pageSize","10000");
+        RequestUtils.sendPostRequest(Api.APPLY_ADD_ME_LIST, params, new ResponseCallBack<Friend>() {
+            @Override
+            public void onSuccessList(List<Friend> data) {
+                super.onSuccessList(data);
+                if(page.equals("1")){
+                    adapter.clear();
+                }
+                adapter.addList(data);
+            }
+
+            @Override
+            public void onFailure(ServiceException e) {
+                CommonHelper.showTip(NewApplyFriendActivity.this,e.getMessage());
+            }
+        }, Friend.class);
+    }
+
+
+    private void initView() {
+        commonTitle.setActivity(this);
+        commonTitle.setTitleText(getResources().getString(R.string.newFriend));
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                ForwardUtils.target(NewApplyFriendActivity.this, Constant.CHAT_MSG,null);
+            }
+        });
+
+        adapter = new NewApplyFriendAdapter(NewApplyFriendActivity.this);
+        mListView.setAdapter(adapter);
+
+
+
+        //滑动菜单
+        initSwipeMenu();
+    }
+
+
+    /**
+     * 滑动菜单
+     */
+    private void initSwipeMenu() {
+        // step 1. create a MenuCreator
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(NewApplyFriendActivity.this);
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(248,
+                        79, 130)));
+                // set item width
+                deleteItem.setWidth(WidgetUtil.dip2px(NewApplyFriendActivity.this,60));
+                // set a icon
+                deleteItem.setIcon(R.mipmap.v3_item_remove);
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+        // set creator
+        mListView.setMenuCreator(creator);
+
+        // step 2. listener item click event
+        mListView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                Friend f = (Friend) adapter.getItem(position);
+                switch (index) {
+                    case 0:
+                        CommonHelper.showTip(NewApplyFriendActivity.this,"delete"+f.getNickName());
+                        break;
+                }
+                return false;
+            }
+        });
+
+
+    }
+
+
+    public void refresh() {
+        loadData();
+    }
+}
---
+++ app/src/main/java/com/huixiangtv/liveshow/adapter/NewApplyGroupdAdapter.java
@@ -0,0 +1,193 @@
+package com.huixiangtv.liveshow.adapter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.huixiangtv.liveshow.Api;
+import com.huixiangtv.liveshow.R;
+import com.huixiangtv.liveshow.activity.NewApplyFriendActivity;
+import com.huixiangtv.liveshow.activity.NewApplyGroupActivity;
+import com.huixiangtv.liveshow.model.Friend;
+import com.huixiangtv.liveshow.service.RequestUtils;
+import com.huixiangtv.liveshow.service.ResponseCallBack;
+import com.huixiangtv.liveshow.service.ServiceException;
+import com.huixiangtv.liveshow.utils.CommonHelper;
+import com.huixiangtv.liveshow.utils.image.ImageUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by hjw on 16/5/13.
+ */
+public class NewApplyGroupdAdapter extends BaseAdapter {
+
+    private int TYPE_COUNT = 2;
+    //有没有处理过
+    private int NO = 0;
+    private int YES = 1;
+
+
+    Activity activity;
+    Context context;
+    List<Friend> voList = new ArrayList<Friend>();
+
+
+    private Map<Integer, View> viewMap = new HashMap<Integer, View>();
+
+    public NewApplyGroupdAdapter(Activity activity) {
+        this.activity = activity;
+        this.context = activity;
+    }
+
+    public NewApplyGroupdAdapter() {
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Friend friend = (Friend) getItem(position);
+        if(friend.getStatus().equals("0")){
+            return YES;
+        }
+        return NO;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return TYPE_COUNT;
+    }
+
+
+    @Override
+    public int getCount() {
+        return voList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return voList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder holder = null;
+        final Friend friend = (Friend) getItem(position);
+        if(getItemViewType(position)==NO){
+            if(convertView == null)
+            {
+                holder = new ViewHolder();
+                convertView = LayoutInflater.from(context).inflate(R.layout.activity_new_friend_item01, parent, false);
+                holder.tvNickName = (TextView) convertView.findViewById(R.id.tvNickName);
+                holder.ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+                holder.tvStatus =(TextView) convertView.findViewById(R.id.tvStatus);
+                convertView.setTag(holder);
+            }else{
+                holder = (ViewHolder)convertView.getTag();
+            }
+            if(friend.getStatus().equals("1")){
+                holder.tvStatus.setText("拒绝");
+            }else if(friend.getStatus().equals("2")){
+                holder.tvStatus.setText("同意");
+            }
+        }else if(getItemViewType(position)==YES){
+            if(convertView == null)
+            {
+                holder = new ViewHolder();
+                convertView = LayoutInflater.from(context).inflate(R.layout.activity_new_friend_item02, parent, false);
+                holder.tvNickName = (TextView) convertView.findViewById(R.id.tvNickName);
+                holder.ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+                holder.tvValues =(TextView) convertView.findViewById(R.id.tvValues);
+                holder.tvMsg =(TextView) convertView.findViewById(R.id.tvMsg);
+                holder.rlAdd =(RelativeLayout) convertView.findViewById(R.id.rlAdd);
+                convertView.setTag(holder);
+            }else{
+                holder = (ViewHolder)convertView.getTag();
+            }
+            holder.rlAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //添加好友
+                    replyAddFriend(friend);
+                }
+            });
+            holder.tvValues.setText("贡献值:"+1987);
+            holder.tvMsg.setText(friend.getReplyContent());
+        }
+
+
+
+        //设置消息用户信息
+        holder.tvNickName.setText(friend.getNickName());
+        ImageUtils.displayAvator(holder.ivPhoto,friend.getPhoto());
+
+
+        return convertView;
+    }
+
+    private void replyAddFriend(final Friend friend) {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("type","2");
+        params.put("content","");
+        params.put("fid",friend.getUid());
+        params.put("gid",friend.getGid());
+        RequestUtils.sendPostRequest(Api.REPLY_ADD_GROUP, params, new ResponseCallBack<Friend>() {
+            @Override
+            public void onSuccess(Friend data) {
+                super.onSuccess(data);
+                CommonHelper.showTip(context,"已成功入群");
+                new Handler().postDelayed(new Runnable() {
+                    public void run() {
+                        ((NewApplyGroupActivity)activity).refresh();
+                    }
+                }, 1000);
+            }
+
+            @Override
+            public void onFailure(ServiceException e) {
+                CommonHelper.showTip(context,e.getMessage());
+            }
+        }, Friend.class);
+    }
+
+    public void clear() {
+        voList.clear();
+        notifyDataSetChanged();
+    }
+
+    public void addList(List<Friend> ls) {
+        if (ls != null) {
+            voList.addAll(ls);
+        }
+        notifyDataSetChanged();
+    }
+
+
+
+    static class ViewHolder
+    {
+
+        public TextView tvNickName;
+        public ImageView ivPhoto;
+        public TextView tvStatus;
+        public RelativeLayout rlAdd;
+        public TextView tvMsg;
+        public TextView tvValues;
+    }
+}
